c6af25f194c169fe6ea2f1513254c33361d6cb75
강유정 <kos9811@naver.com>
2021-03-16 18:20:07 +0900

    redux 개요

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux'
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducers/index'
 
+// 그냥 store 는 객체 밖에 못 받으니까 promise, function도 받을 수 있게 middleware 와 함께 store 만든다.
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
+  <App />
+  </Provider>
+  
+  , document.getElementById('root')
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
